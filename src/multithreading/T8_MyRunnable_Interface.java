4ba2ef0fbeae9cf7b7f336a9ba0133bd30ec0864
AbhishekHulule9579 <abhishekhulule.sknsits.it@gmail.com>
2026-01-04 14:39:37 +0530

    4 jan

--- src/multithreading/T8_MyRunnable_Interface.java
+++ src/multithreading/T8_MyRunnable_Interface.java
@@ -1,19 +1,21 @@
 package multithreading;
 
-class myRunnable implements Runnable{
+class myRunnable implements Runnable{  // defining the thread
     public void run(){
         for(int i=0;i<5;i++){
             System.out.println(i);
-        }
+        }  //job of the thread
+        //executed by the child thread
     }
 }
 public class T8_MyRunnable_Interface {
     public static void main(String[] args) {
         myRunnable r=new myRunnable();
-        Thread t=new Thread(r);
+        Thread t=new Thread(r);   //r is the target of the thread
         t.start();
         for(int i=0;i<5;i++){
             System.out.println("**");
-        }
+        }  //executed by the min thread
     }
 }
+//after compeleting this code we will get mixed output and we can't tell exact output .
